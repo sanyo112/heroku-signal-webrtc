a52321414007a2d4ef02f88388dfa22ee69d821b
Pardo Guerrero <jrpardog@indra.es>
2016-06-28 15:46:43 +0200

    add socket io listening

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var _ = require('lodash-node');
+
+var users = [];
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -11,6 +16,54 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
+});
+
+io.on('connection', function (socket) {
+  socket.on('login', function (name) {
+    // if this socket is already connected,
+    // send a failed login message
+    if (_.findIndex(users, { socket: socket.id }) !== -1) {
+      socket.emit('login_error', 'You are already connected.');
+    }
+
+    // if this name is already registered,
+    // send a failed login message
+    if (_.findIndex(users, { name: name }) !== -1) {
+      socket.emit('login_error', 'This name already exists.');
+      return; 
+    }
+
+    users.push({ 
+      name: name,
+      socket: socket.id
+    });
+
+    socket.emit('login_successful', _.pluck(users, 'name'));
+    socket.broadcast.emit('online', name);
+
+    console.log(name + ' logged in');
+  });
+
+  socket.on('sendMessage', function (name, message) {
+    var currentUser = _.find(users, { socket: socket.id });
+    if (!currentUser) { return; }
+
+    var contact = _.find(users, { name: name });
+    if (!contact) { return; }
+    
+    io.to(contact.socket)
+      .emit('messageReceived', currentUser.name, message);
+  });
+
+  socket.on('disconnect', function () {
+    var index = _.findIndex(users, { socket: socket.id });
+    if (index !== -1) {
+      socket.broadcast.emit('offline', users[index].name);
+      console.log(users[index].name + ' disconnected');
+
+      users.splice(index, 1);
+    }
+  });
 });
 
 app.listen(app.get('port'), function() {
